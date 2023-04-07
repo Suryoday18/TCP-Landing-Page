d9576173d2050755440245d956225dcedd450149
Suryoday18 <suryoday.yadav@transerve.com>
2023-04-07 06:20:23 +0530

    Changes in govt strip, logo adjusting

--- src/Components/GovtStrip.js
+++ src/Components/GovtStrip.js
@@ -5,14 +5,17 @@ const GovtStrip = () => {
   return (
     <Box sx={{ height: 180, display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row', bgcolor: '#fff', color: 'black' }}>
         <Box sx={{ display: 'flex', flexDirection: 'row'}}>
+          
+          <div style={{display: 'flex', flexDirection: 'row', color: '#1F4A7C', width: '620px', height: 'auto', border: '7px solid #1F4A7C', position:'relative'}}>
+          <div>
           <img src="http://www.ud.hp.gov.in/sites/default/files/inline-images/logo1.png" alt="image2" style={{ width: '90px', height: '90px', margin: '20px 30px 20px 30px' }} />
-          <div style={{display: 'flex', flexDirection: 'column', color: '#1F4A7C', width: '500px', height: 'auto', border: '2px solid #1F4A7C', position:'relative'}}>
-          
-          <h4 style={{fontWeight: '30px', margin: '10px', width: '125px', position: 'absolute', top: '-20px', backgroundColor: 'white'}}>हिमाचल प्रदेश सरकार</h4>
+          </div>
+          <div style={{display: 'flex', flexDirection: 'column'}}>
+          <h4 style={{fontWeight: '30px', margin: '10px -135px', width: '125px', position: 'absolute', top: '-20px', backgroundColor: 'white'}}>हिमाचल प्रदेश सरकार</h4>
           <h2 style={{color: '#154773', margin: '20px 10px 10px 10px'}}>नगर एवम् ग्राम योजना विभाग</h2>
-          <h2 style={{fontWeight: '30px', margin: '10px'}}>Town & Country Planning Department </h2>
-          <h6 style={{fontSize: '15.75px', color: '#154773',margin: '10px 10px 10px 180px', position: 'absolute', bottom:'-20px', backgroundColor: 'white'}}>Government of Himanchal Pardesh</h6>
-          
+          <h2 style={{fontSize: '25px', margin: '0px 0px 35px 10px'}}>Town & Country Planning Department </h2>
+          <h6 style={{fontSize: '15.75px', color: '#154773',margin: '10px 10px 10px 195px', position: 'absolute', bottom:'-20px', backgroundColor: 'white'}}>Government of Himanchal Pardesh</h6>
+          </div>
           </div>
         </Box>
       <div style={{margin: '10px'}}>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -18,11 +18,12 @@ const Navbar = () => {
   return (
     <AppBar position="static" style={{background: '#1F4A7C', height: '44px'}} >
       <Toolbar>
-        <Button color='inherit' style={{marginLeft: '120px', marginBottom: '18px', fontSize: '12px', color: 'white'}}><FcHome style={{width: '30px', height: '30px'}}/></Button>
+        <Button color='inherit' style={{marginLeft: '70px', marginBottom: '18px', fontSize: '12px', color: 'white'}}><FcHome style={{width: '30px', height: '30px'}}/></Button>
         <Button color="inherit" style={{marginLeft: '30px', marginBottom: '18px', fontSize: '12px', color: 'white'}}>About Us</Button>
         <Button color="inherit" style={{marginLeft: '30px', marginBottom: '18px',  color: 'white'}}>Planning Regulations</Button>
         <Button color="inherit" onClick={handleClick} style={{marginLeft: '30px', marginBottom: '18px', fontSize: '12px', color: 'white'}}>Development Plan</Button>
         <Button color='inherit' style={{marginLeft: '30px', marginBottom: '18px', fontSize: '12px', color: 'white'}}>Notifications / Orders</Button>
+        <Button color='inherit' style={{marginLeft: '30px', marginBottom: '18px', fontSize: '12px', color: 'white'}}>Act & Rules</Button>
         <Button color="inherit" style={{marginLeft: '30px', marginBottom: '18px',  fontSize: '12px', color: 'white'}}>Media</Button>
         {/* <Button color="inherit" style={{marginLeft: '20px', fontSize: '12px'}}>Act & Rules</Button> */}
         <Button color="inherit" style={{marginLeft: '30px', marginBottom: '18px',  fontSize: '12px', color: 'white'}}>RTI</Button>
